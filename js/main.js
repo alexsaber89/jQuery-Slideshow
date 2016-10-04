3d66ce04ce3182a8b02ca6338afeb93bfdc55cd5
Alex Saber <Alex Saber>
2016-10-03 21:15:11 -0500

    prev button working but i need to adjust the counter variable value to properly cycle through images in order

--- js/main.js
+++ js/main.js
@@ -1,12 +1,39 @@
-'use strict';
 var container = $("#container");
-var imgArray = ["../img/chalkley_1.png","../img/chalkley_2.png","../img/chalkley_3.png","../img/chalkley_4.png","../img/guil_1.png","../img/guil_2.png"];
 var counter = 0;
+var imgArray = [
+  "../img/chalkley_1.png",
+  "../img/chalkley_2.png",
+  "../img/chalkley_3.png",
+  "../img/chalkley_4.png",
+  "../img/guil_1.png",
+  "../img/guil_2.jpg"
+];
+var zeroIndexNumOfPicsInArray = imgArray.length - 1;
 
-function getFiles(array){
-  container.html("<img src='" + array[counter] + "'>");
+function displayImage(array) {
+  container.html(
+    "<button class='prev_button'><span class='glyphicon glyphicon-arrow-left'></span></button>" +
+      "<img src='" +
+        array[counter] +
+      "'>" +
+    "<button class='next_button'><span class='glyphicon glyphicon-arrow-right'></button>"
+  );
   console.log("<img src='" + array[counter] + "'>");
-  counter++;
 }
 
-getFiles(imgArray);
+function previousImage() {
+  console.log("previous image");
+  console.log(counter);
+  counter = zeroIndexNumOfPicsInArray - counter;
+  console.log(counter);
+  displayImage(imgArray);
+}
+
+function nextImage() {
+  console.log("next image");
+}
+
+displayImage(imgArray);
+
+$(".prev_button").click(previousImage);
+$(".next_button").click(nextImage);
